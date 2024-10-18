1cd8b49b31ad9a80a55bc63e044bac9b2afc1585
GabrielHenriqueRibas <ghenriqueribas@gmail.com>
2024-10-18 09:17:27 -0300

    Atualização no cabeçalho

--- js/index.js
+++ js/index.js
@@ -1,8 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.getElementById('menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.getElementById('text');
+    const text = "Prometi a Deus que até meu último suspiro seria para os jovens (São João Bosco).";
+    let index = 0;
+    let isDeleting = false;
+    let deleteIndex = text.length;
 
-    menuButton.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-    });
+    function writeText() {
+        if (!isDeleting) {
+            if (index < text.length) {
+                textElement.innerHTML += text.charAt(index);
+                index++;
+                setTimeout(writeText, 100);
+            } else {
+                
+                setTimeout(deleteText, 2000); 
+            }
+        }
+    }
+
+    function deleteText() {
+        if (deleteIndex > 0) {
+            textElement.innerHTML = text.substring(0, deleteIndex - 1);
+            deleteIndex--;
+            setTimeout(deleteText, 100);
+        } else {
+            index = 0;
+            deleteIndex = text.length;
+            setTimeout(writeText, 1000); 
+        }
+    }
+
+    writeText();
 });
